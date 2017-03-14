1161c0fa83c300a1253098284fde41d93313808f
DengMins <1253788859@qq.com>
2017-03-14 21:29:14 +0800

    DengMin

--- src/com/xin/controller/StudentController.java
+++ src/com/xin/controller/StudentController.java
@@ -143,6 +143,7 @@ public class StudentController extends BaseController{
     @RequestMapping("/add")
     @ResponseBody
     public Object add(Student student){
+    	System.out.println("User"+student.getUserId());
 		Student stu = studentService.selectByNo();
     	String No = StudentNo.getNo(stu);
     	student.setStudentNo(No);
