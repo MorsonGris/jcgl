c27dc37cd2a2b58d65ad62d549b39187ed95e0ae
Libra-json <3140378543@qq.com>
2017-03-24 21:30:11 +0800

    bmg
    123

--- WebContent/static/proscenium/js/app.js
+++ WebContent/static/proscenium/js/app.js
@@ -3,8 +3,25 @@ $(document).ready(function() {
         document.getElementById('light').style.display='block';
         /* document.getElementById('fade').style.display='block'; */
     })
+    $("#contact2").click(function () {
+        document.getElementById('light').style.display='block';
+        /* document.getElementById('fade').style.display='block'; */
+    })
     $("#closePhone").click(function () {
         document.getElementById('light').style.display='none';
+        /* document.getElementById('fade').style.display='none'; */
+    })   
+	$.goup({
+        trigger: 100,
+        bottomOffset: 250,
+        locationOffset: 2,
+    })
+    $("#maps").click(function () {
+        document.getElementById('map').style.display='block';
+        /* document.getElementById('fade').style.display='block'; */
+    })
+    $("#closeMap").click(function () {
+        document.getElementById('map').style.display='none';
         /* document.getElementById('fade').style.display='none'; */
     })   
 	$.goup({
